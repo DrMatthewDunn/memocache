196dad8749a4f9a3df55d6d1cb768e88ab6b1398
DrMatthewDunn <matthew@sayitvisually.com>
2020-06-04 11:11:14 -0700

    Update - eliminate expiringdict
    Performance quirks with expiringdict in production; not worth debugging, easy workaround with standard dict.

--- memocache.py
+++ memocache.py
@@ -1,15 +1,23 @@
 ## -*- coding: utf-8 -*-
 from google.appengine.api import memcache
 from memoization import cached #https://github.com/lonelyenvoy/python-memoization
-from expiringdict import ExpiringDict
+#from expiringdict import ExpiringDict eliminated -- perf was quirky
 from datetime import datetime
 
 
 #dict of keys that have been memo'd
-keycache = ExpiringDict(max_len=1000,max_age_seconds=3600)
+#keycache = ExpiringDict(max_len=1000,max_age_seconds=3600)
+keycache = {}
 
+def check_keycache():
+    cache_max = 1000
+    if len(keycache) > cache_max:
+        top = len(keycache) - int(cache_max / 10) #10% padding
+        keep = sorted(keycache.values(),reverse=True)[0:top]
+        newcache = {key:value for key,value in keycache.iteritems() if value in keep}
+        keycache.update(newcache)
 
-@cached(ttl=59)  #set TTL to suit
+@cached(ttl=100,thread_safe=False) 
 def memcacheget(key):
     value = memcache.get(key)
     return value
@@ -25,16 +33,16 @@ class memocache():
     '''
    
     def __init__(self):
+        self.cache_max = 1000
         pass
 
     @classmethod
     def get(cls,key):
-        '''
-        Retrieve from cache; memo first, then memcache, else None.
-        '''
         learned = keycache.get(key,None)
-        if learned: #key already in memcache so it'll actually retrieve from memo
+        if learned: #key already in memcache so will actually retrieve from memo
             value = memcacheget(key)
+            if not value:
+                keycache.pop(key,None)
             return value
         else:  #key not learned or forgotten
             value = memcache.get(key)
@@ -46,20 +54,21 @@ class memocache():
 
     @classmethod
     def add(cls,key=None,value=None,time=None):
-        '''
-        Add value to cache, add key to keycache
-        '''
-        keycache[key] = int((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds()) #add key to dictionary of memo'd keys
-        if time:
-            memcache.add(key,value,time=time)
+        check_keycache()
+        if all([key,value]):
+            keycache[key] = int((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds()) #add key to dictionary of memo'd keys
+            if time:
+                memcache.add(key,value,time=time)
+            else:
+                memcache.add(key,value) #DS record permanent; memo cache still only 59 seconds
+            return True
         else:
-            memcache.add(key,value) #DS record permanent; memo cache still only 59 seconds
-        return
+            return False
+
 
     @classmethod
     def delete(cls,key):
-        '''
-        Remove key from keycache, value from memcache
-        '''
         keycache.pop(key)
         memcache.delete(key)
+
+
